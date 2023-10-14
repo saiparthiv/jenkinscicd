bb4d01a34fb14cd716d8bc4a473740ff21298679
saiparthiv <saiparthiv95@gmail.com>
2023-10-13 23:54:59 -0500

    Modified Jenkinsfile

--- jenkins_ecs.tf
+++ jenkins_ecs.tf
@@ -120,6 +120,27 @@ resource "aws_lb_listener" "web" {
   }
 }
 
+# Create a Target Group
+resource "aws_lb_target_group" "jenkinscicd_target_group" {
+  name     = "jenkinscicd-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "vpc-08e4f792b45a68d24" # Use your VPC ID
+  health_check {
+    path                = "/" # Replace with an appropriate health check path
+    interval            = 30
+    timeout             = 10
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# Associate your Fargate service with the ALB's target group
+resource "aws_lb_target_group_attachment" "jenkinscicd_task_attachment" {
+  target_group_arn = aws_lb_target_group.jenkinscicd_target_group.arn
+  target_id        = aws_ecs_task_definition.jenkinscicd_task.arn
+  port             = 80
+}
 
 
 # IAM Policy for ECS Execution Role
