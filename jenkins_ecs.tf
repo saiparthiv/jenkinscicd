9f1ef92ec059ecd9e3da603cf02d2659d9aa9efd
saiparthiv <saiparthiv95@gmail.com>
2023-10-14 18:27:39 -0500

    Modified Jenkinsfile

--- jenkins_ecs.tf
+++ jenkins_ecs.tf
@@ -1,39 +1,67 @@
+# Define the AWS provider and region
 provider "aws" {
   region = "us-east-1"
 }
 
+# ECS Cluster
 resource "aws_ecs_cluster" "jenkinscicd_cluster" {
   name = "jenkinscicd-cluster"
 }
 
-resource "aws_ecs_task_definition" "my_task" {
-  family = "my-task"
-  network_mode = "awsvpc"
+# Task Definition
+resource "aws_ecs_task_definition" "jenkinscicd_task" {
+  family                   = "jenkinscicd-task"
+  network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
 
-  cpu = "256"
-  memory = "512"  
+  execution_role_arn = aws_iam_role.ecs_execution_role.arn
 
-  container_definitions = <<EOF
-[
-  {
-    "name": "jenkinscicd-app",
-    "image": "805619463928.dkr.ecr.us-east-1.amazonaws.com/jenkinscicd:latest",
-    "portMappings": [
-      {
-        "containerPort": 85,
-        "hostPort": 85
-      }
-    ]
-  }
-]
+  cpu = "256"
+  memory = "512"
+
+  container_definitions = <<DEFINITION
+  [
+    {
+      "name": "jenkinscicd-app",
+      "image": "805619463928.dkr.ecr.us-east-1.amazonaws.com/jenkinscicd:latest",
+      "memory": 512,
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 85,
+          "hostPort": 85
+        }
+      ]
+    }
+  ]
+  DEFINITION
+}
+
+# IAM Role for ECS Execution
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "jenkinscicd-ecs-execution-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
 EOF
 }
 
+# ECS Service
 resource "aws_ecs_service" "jenkinscicd_service" {
-  name = "jenkinscicd-service"
-  cluster = aws_ecs_cluster.jenkinscicd_cluster.name
-  task_definition = aws_ecs_task_definition.my_task.arn
+  name            = "jenkinscicd-service"
+  cluster         = aws_ecs_cluster.jenkinscicd_cluster.id
+  task_definition = aws_ecs_task_definition.jenkinscicd_task.arn
   launch_type     = "FARGATE"
   network_configuration {
     subnets = ["subnet-0c80f601a78574913"] # Replace with your subnet IDs
@@ -41,4 +69,53 @@ resource "aws_ecs_service" "jenkinscicd_service" {
   }
   desired_count = 1
   depends_on = [aws_ecs_cluster.jenkinscicd_cluster]
+}
+
+# ECR Repository Data Source
+data "aws_ecr_image" "jenkinscicd_image" {
+  repository_name = "jenkinscicd"
+  image_tag = "latest"
+}
+
+
+# IAM Policy for ECS Execution Role
+resource "aws_iam_policy" "ecs_execution_policy" {
+  name = "jenkinscicd-ecs-execution-policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ecr:GetAuthorizationToken",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:GetRepositoryPolicy",
+        "ecr:ListImages",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetLifecyclePolicy",
+        "ecr:GetLifecyclePolicyPreview",
+        "ecr:PutImage"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+# Attach ECS Execution Role Policy
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_attachment" {
+  policy_arn = aws_iam_policy.ecs_execution_policy.arn
+  role       = aws_iam_role.ecs_execution_role.name
+}
+
+# Output the ECS Cluster and Service details
+output "ecs_cluster_id" {
+  value = aws_ecs_cluster.jenkinscicd_cluster.id
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.jenkinscicd_service.name
 }
