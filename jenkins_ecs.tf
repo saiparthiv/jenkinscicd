57d8ca60b0d740e3ccac451198e03f162e5ff78f
saiparthiv <saiparthiv95@gmail.com>
2023-10-14 18:19:22 -0500

    Modified Jenkinsfile

--- jenkins_ecs.tf
+++ jenkins_ecs.tf
@@ -30,8 +30,14 @@ EOF
 }
 
 resource "aws_ecs_service" "jenkinscicd_service" {
-  name = "my-service"
+  name = "jenkinscicd-service"
   cluster = aws_ecs_cluster.jenkinscicd_cluster.name
   task_definition = aws_ecs_task_definition.my_task.arn
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets = ["subnet-0c80f601a78574913"] # Replace with your subnet IDs
+    security_groups = ["sg-008bb6cdd38c35d75"] # Replace with your security group IDs
+  }
   desired_count = 1
+  depends_on = [aws_ecs_cluster.jenkinscicd_cluster]
 }
