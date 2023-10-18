c1384e582bb8f88721455b20cd92518113dcb376
saiparthiv <saiparthiv95@gmail.com>
2023-10-18 00:47:55 -0500

    Made some changes

--- jenkins_ecs.tf
+++ jenkins_ecs.tf
@@ -3,6 +3,68 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Define an Internet Gateway resource
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Attach the Internet Gateway to the VPC
+resource "aws_vpc_attachment" "main_igw_attachment" {
+  vpc_id             = aws_vpc.main.id
+  internet_gateway_id = aws_internet_gateway.main.id
+}
+
+
+# Create subnets within the VPC
+resource "aws_subnet" "subnet_a" {
+  count = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.${count.index * 16}/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+# Define a security group for your ECS tasks
+resource "aws_security_group" "ecs_security_group" {
+  name_prefix = "ecs-security-group-"
+  description = "Security group for ECS tasks"
+  vpc_id      = aws_vpc.main.id
+
+  # Define inbound and outbound rules as needed
+  # For example, allow incoming traffic on port 80
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Adjust the CIDR block as needed
+  }
+
+  # You can define more rules as required.
+}
+
+# Define a route table for your subnets
+resource "aws_route_table" "subnet_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  # Define routes as needed, such as a route to an internet gateway
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id  # You'll need to define an Internet Gateway resource
+  }
+}
+
+# Associate your subnets with the route table
+resource "aws_route_table_association" "subnet_route_association" {
+  count          = length(aws_subnet.subnet_a)
+  subnet_id      = aws_subnet.subnet_a[count.index].id
+  route_table_id = aws_route_table.subnet_route_table.id
+}
+
 # ECS Cluster
 resource "aws_ecs_cluster" "jenkinscicd_cluster" {
   name = "jenkinscicd-cluster"
@@ -57,14 +119,10 @@ resource "aws_iam_role" "ecs_execution_role" {
 EOF
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
 resource "aws_alb" "jenkinscicd_alb" {
   name = "jenkinscicd-alb"
-  subnets = ["subnet-03899ca854bdc5261", "subnet-08dd2327ec620088b"] # Replace with your subnet IDs
-  security_groups = ["sg-0195c7c8f09395100"] # Replace with your security group IDs
+  subnets = aws_subnet.subnet_a[*].id
+  security_groups = [aws_security_group.ecs_security_group.id]  # Use the security group created earlier
 }
 
 resource "aws_alb_target_group" "jenkinscicd_target_group" {
@@ -105,8 +163,8 @@ resource "aws_ecs_service" "jenkinscicd_service" {
   task_definition = aws_ecs_task_definition.jenkinscicd_task.arn
   launch_type     = "FARGATE"
   network_configuration {
-    subnets = ["subnet-03899ca854bdc5261"] # Replace with your subnet IDs
-    security_groups = ["sg-0195c7c8f09395100"] # Replace with your security group IDs
+    subnets = aws_subnet.subnet_a[*].id
+    security_groups = [aws_security_group.ecs_security_group.id]  # Use the security group created earlier
     assign_public_ip = "true"
   }
   desired_count = 1
@@ -118,7 +176,6 @@ data "aws_ecr_image" "jenkinscicd_image" {
   repository_name = "jenkinscicd"
   image_tag = "latest"
 }
-
 
 # IAM Policy for ECS Execution Role
 resource "aws_iam_policy" "ecs_execution_policy" {
