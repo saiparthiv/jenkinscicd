547ee9836d7e9318e5012f152c66338a39ed4560
saiparthiv <saiparthiv95@gmail.com>
2023-10-18 01:09:30 -0500

    Made some changes

--- jenkins_ecs.tf
+++ jenkins_ecs.tf
@@ -22,10 +22,16 @@ resource "aws_internet_gateway_attachment" "main_igw_attachment" {
 
 # Create subnets within the VPC
 resource "aws_subnet" "subnet_a" {
-  count = 2
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.${count.index * 16}/24"
-  availability_zone       = "us-east-1a"
+  vpc_id         = aws_vpc.main.id
+  cidr_block       = "10.0.1.0/24"
+  availability_zone    = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id         = aws_vpc.main.id
+  cidr_block       = "10.0.2.0/24"
+  availability_zone    = "us-east-1b"
   map_public_ip_on_launch = true
 }
 
